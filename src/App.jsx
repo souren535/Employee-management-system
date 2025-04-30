a412775c8d2ce3e2bec0f92fba239f4ff2e8a0b0
souren <souren.cool535@outlook.com>
2025-04-30 11:29:40 +0530

    30 apr

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import AuthContext from "./context/AuthContext";
+import { TaskContext } from "./context/TaskContext";
 
 const App = () => {
   return (
     <>
-      {/* <Login /> */}
+      <AuthContext>
+        <TaskContext>
+          <Login />
+        </TaskContext>
+      </AuthContext>
       {/* <EmployeeDashboard /> */}
-      <AdminDashboard/>
+      {/* <AdminDashboard/> */}
     </>
   );
 };
---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TaskContext = ({ children }) => {
+  return <div>{children}</div>;
+};
